9c0e57a75dea99841d78ada3710402d72c91df09
vilma catani <vilma.catani@gmail.com>
2023-03-06 16:57:06 +0100

    created user controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_145649) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_153930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,8 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_145649) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "company_name"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_type"
+    t.string "address"
+    t.string "city"
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
