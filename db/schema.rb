00e5d90dab5fada916f8e653c020e87c1f32ad95
vilma catani <vilma.catani@gmail.com>
2023-03-06 17:08:38 +0100

    created the startups model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_153930) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_160628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "startups", force: :cascade do |t|
+    t.string "funding"
+    t.integer "team"
+    t.date "funding_round_end_date"
+    t.float "funding_amount"
+    t.string "industry"
+    t.integer "headcount"
+    t.integer "turnover"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_startups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_153930) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "startups", "users"
 end
