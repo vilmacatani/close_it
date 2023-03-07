e2f1dfb7d80c3da88f35088e4e9a09289787c838
vilma catani <vilma.catani@gmail.com>
2023-03-07 12:24:59 +0100

    created members model

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,4 @@
 class Startup < ApplicationRecord
   belongs_to :user
+  has_many :members
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_104449) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_112418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_104449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["connection_id"], name: "index_meetings_on_connection_id"
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "bio"
+    t.string "position"
+    t.bigint "startup_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["startup_id"], name: "index_members_on_startup_id"
   end
 
   create_table "startups", force: :cascade do |t|
@@ -85,5 +96,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_104449) do
   add_foreign_key "connections", "users", column: "sender_id"
   add_foreign_key "investors", "users"
   add_foreign_key "meetings", "connections"
+  add_foreign_key "members", "startups"
   add_foreign_key "startups", "users"
 end
