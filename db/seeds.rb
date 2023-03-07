0eda57d01675da6156453b11a2f1ddc3af6e9b47
(Maria Massano) mariamassano6@gmail.com <mariamassano6@gmail.com>
2023-03-07 17:23:11 +0100

    changes in the navbard when logged in

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(email: "vilmacatani@gmail.com", password: "111111", user_type: 'investor')
+User.create!(email: "martacatani@gmail.com", password: "111111", user_type: 'startup')
