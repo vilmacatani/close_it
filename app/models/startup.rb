40c53f175f79d392467d38a0dd7fe7afe6173c2f
aurelabode <lelabode@hotmail.com>
2023-03-08 12:05:20 +0100

    db seed done

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,5 +1,5 @@
 class Startup < ApplicationRecord
   belongs_to :user
-  validates :funding, :funding_round_end, :funding_amount, :industry, :user_id, :headcount, :turnover, presence: true
-  has_many :members
+  validates :funding, :funding_round_end_date, :funding_amount, :industry, :user_id, :headcount, :turnover, presence: true
+  has_many :members, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Connection.destroy_all
+User.destroy_all
+
 funding = ["Seed", "Pre-Seed", "Series A", "Series B", "Series C"]
 5.times do
   user = User.create!(
@@ -15,7 +18,9 @@ funding = ["Seed", "Pre-Seed", "Series A", "Series B", "Series C"]
     company_name: Faker::Company.name,
     city: Faker::Address.city_suffix,
     country: Faker::Address.country,
-    user_type: "investor"
+    user_type: "investor",
+    email: Faker::Internet.email,
+    password: "111111"
   )
 
   Investor.create!(
@@ -35,7 +40,9 @@ end
     company_name: Faker::Company.name,
     city: Faker::Address.city_suffix,
     country: Faker::Address.country,
-    user_type: "startup"
+    user_type: "startup",
+    email: Faker::Internet.email,
+    password: "111111"
   )
 
   Startup.create!(
@@ -43,11 +50,41 @@ end
     funding_amount: Faker::Number.between(from: 1, to: 10),
     funding_round_end_date: Faker::Date.between(from: '2023-01-01', to: '2024-12-31'),
     funding: funding.sample,
-    team: Faker::Number.number,
+    team: Faker::Number.between(from: 1, to: 10),
     industry: Faker::Company.industry,
     headcount: Faker::Number.between(from: 1, to: 500),
     turnover: Faker::Number.between(from: 1, to: 200)
   )
 end
 
+user_ids = User.all.pluck(:id)
 
+5.times do
+  connection = Connection.create!(
+    pending: true,
+    accepted: true,
+    message: Faker::Quote.matz,
+    sender_id: user_ids.sample,
+    receiver_id: user_ids.sample,
+    connection_type: "hello"
+  )
+
+  Meeting.create!(
+    meeting_time: DateTime.new,
+    connection_id: connection.id,
+    meeting_accepted: true,
+    meeting_pending: true
+  )
+end
+
+startup_ids = Startup.all.pluck(:id)
+
+6.times do
+  Member.create!(
+    startup_id: startup_ids.sample,
+    first_name: Faker::Name.first_name,
+    last_name:  Faker::Name.last_name,
+    bio: Faker::Company.industry,
+    position: Faker::Company.bs
+  )
+end
