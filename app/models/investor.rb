970c078b87c6687cfd5ff2bed512eac08746cd8b
vilma catani <vilma.catani@gmail.com>
2023-03-09 15:33:42 +0100

    fixed the view

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,4 +1,8 @@
 class Investor < ApplicationRecord
   belongs_to :user
   # validates :private, :funding_type, :investor_type, :user_id, presence: true
+  after_create :set_type
+  def set_type
+    self.user.update(user_type: "investor")
+  end
 end
