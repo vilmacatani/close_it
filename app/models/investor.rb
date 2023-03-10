98dd4e4aaeb4520c359985fed5b5e6f049421071
(Maria Massano) mariamassano6@gmail.com <mariamassano6@gmail.com>
2023-03-10 14:39:09 +0100

    pushing

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,13 +1,10 @@
 class Investor < ApplicationRecord
   INVESTORS = ["Angel Investor", "Peer-to-Peer Lenders", "Personal Investors", "Banks", "Venture Capitalists"]
-  FUNDINGS = ["Series A", "Series B", "Series C", "Series D"]
+  FUNDINGS = ["Pree-Seed", "Seed", "Series A", "Series B", "Series C", "Series D"]
   belongs_to :user
-  INVESTORS = ["Angel Investor", "Peer-to-Peer Lenders", "Personal Investors", "Banks", "Venture Capitalists"]
-  FUNDINGS = ["Series A", "Series B", "Series C", "Series D"]
+
   # validates :private, :funding_type, :investor_type, :user_id, presence: true
-  validates :investor_type, inclusion: { in: INVESTORS }
-  validates :funding_type, inclusion: { in: FUNDINGS }
-  
+
   after_create :set_type
   validates :investor_type, inclusion: { in: INVESTORS }
   validates :funding_type, inclusion: { in: FUNDINGS }
